94fc17ea25d95924950cbe1de0ee595874914e9e
LuizHenz <robertgito@hotmail.com>
2023-08-28 13:34:33 -0300

    up

---
+++ Desafios/Desafio9.py
@@ -0,0 +1,26 @@
+cpf = '09701739973'
+nove_validos = cpf[:9]
+contador = 10
+resultado = 0
+for digito in nove_validos:
+    resultado += int(digito)*contador
+    contador -= 1
+result = (resultado * 10) % 11
+primeiro_digito = 0 if result > 9 else result
+
+
+dez_validos = nove_validos + str(primeiro_digito)
+contador_dois = 11
+resultado_dois = 0
+for digito in dez_validos:
+    resultado_dois += int(digito)*contador_dois
+    contador_dois -= 1
+result_dois = (resultado_dois * 10) % 11
+segundo_digito = 0 if result_dois > 9 else result_dois
+
+cpf_validado = f'{nove_validos}{primeiro_digito}{segundo_digito}'
+
+if cpf_validado == cpf:
+    print(f'{cpf_validado} é valido.')
+else:
+    print('CPF inválido.')
